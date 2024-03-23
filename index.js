ed9fa4a486c48f85dad1501d59d6c11f812b7250
dominique Taplin <Dtaplin21@aol.com>
2024-03-23 09:04:44 -0700

    bs push

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ function calculate(){
     display.value = eval(display.value);
     } 
     catch(error){
-        display.value = "error";
+        display.value = "Not available";
+    }
+}
+
+function appendToDisplay(input){
+    let displayValue =  dispaly.value;
+    let lastChar = displayValue[displayValue.length - 1]
+    if(isNaN(lastChar)){
+        if(input === "+"){
+            display.value += input;
+        } else {
+            display.value += input;
+        } 
+    } else {
+        display.value += input;
     }
 }
